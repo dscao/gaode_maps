f98b3d6e389a5df2366ba103c7b66c07ce576680
dscao <dscao.com@gmail.com>
2024-07-19 11:15:01 +0800

    Fix static path config for HA 2024.7+

--- custom_components/gaode_maps/__init__.py
+++ custom_components/gaode_maps/__init__.py
@@ -2,7 +2,9 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-from homeassistant.components import websocket_api
+from homeassistant.components import websocket_api, frontend
+from homeassistant.components.http import StaticPathConfig
+
 import datetime
 import logging
 
@@ -33,16 +35,20 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         devicetrackeridlist = ','.join(trackerids)
     else:
         devicetrackeridlist = ''
-    hass.http.register_static_path("/gaode_maps_www", hass.config.path("custom_components/" + DOMAIN + "/www"), False)
-    hass.components.frontend.async_register_built_in_panel(
-                        "iframe",
-                        "墨澜地图",
-                        "mdi:map",
-                        DOMAIN,
-                        { "url": f"/gaode_maps_www/index.html?hasstoken={hasstoken}&gaodekey={gaodekey}&jscode={jscode}&devicetrackeridlist={devicetrackeridlist}&v={VERSION}" },
-                        require_admin=False)
+    await hass.http.async_register_static_paths([
+        StaticPathConfig("/gaode_maps_www", hass.config.path("custom_components/" + DOMAIN + "/www"), False)
+    ])
+    frontend.async_register_built_in_panel(
+        hass,
+        "iframe",
+        "墨澜地图",
+        "mdi:map",
+        DOMAIN,
+        { "url": f"/gaode_maps_www/index.html?hasstoken={hasstoken}&gaodekey={gaodekey}&jscode={jscode}&devicetrackeridlist={devicetrackeridlist}&v={VERSION}" },
+        require_admin=False
+    )
                         
-    hass.components.frontend.add_extra_js_url(hass, f'/gaode_maps_www/map.js?v={VERSION}')
+    frontend.add_extra_js_url(hass, f'/gaode_maps_www/map.js?v={VERSION}')
     
     def receive_data(hass, connection, msg):
         data = msg['data']
@@ -59,7 +65,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
                 )
             )
 
-    hass.components.websocket_api.async_register_command(
+    websocket_api.async_register_command(
+            hass,
             DOMAIN,
             receive_data,
             SCHEMA_WEBSOCKET
@@ -68,5 +75,5 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    hass.components.frontend.async_remove_panel(DOMAIN)
+    frontend.async_remove_panel(hass, DOMAIN)
     return True
